24567c9f9feecbd03be131918832b4da88c7a403
Christopher Matthies <Christopher.Matthies@desy.de>
2019-04-25 16:33:49 +0200

    Add muon scale factors

--- src/HighPtSingleTopMainSelectionModule.cxx
+++ src/HighPtSingleTopMainSelectionModule.cxx
@@ -23,18 +23,18 @@ namespace uhh2 {
     
   private:
     
-    std::unique_ptr<AnalysisModule> sf_lumi, sf_pileup, sf_toppt;
+    std::unique_ptr<AnalysisModule> sf_lumi, sf_pileup, sf_muon_trig, sf_muon_id, sf_muon_iso, sf_muon_trk, sf_toppt;
     std::unique_ptr<AnalysisModule> scale_variation;
     
-    std::unique_ptr<AndHists> hist_sf;
+    std::unique_ptr<AndHists> hist_sf, hist_topptreweighting;
 
     bool is_data, is_mc, is_muon, is_elec, bTopPtReweighting;
     string dataset_version;
-    string syst_PU;
+    string syst_pileup, syst_muon_trigger, syst_muon_id, syst_muon_iso, syst_muon_trk;
   };
 
 
-  HighPtSingleTopMainSelectionModule::HighPtSingleTopMainSelectionModule(Context & ctx){
+  HighPtSingleTopMainSelectionModule::HighPtSingleTopMainSelectionModule(Context & ctx) {
 
     //------//
     // KEYS //
@@ -52,7 +52,11 @@ namespace uhh2 {
 
     bTopPtReweighting = ctx.get("TopPtReweighting") == true;
 
-    syst_PU = ctx.get("SystDirection_PU", "nominal");
+    syst_pileup = ctx.get("SystDirection_Pileup", "nominal");
+    syst_muon_trigger = ctx.get("SystDirection_MuonTrig", "nominal");
+    syst_muon_id = ctx.get("SystDirection_MuonId", "nominal");
+    syst_muon_iso = ctx.get("SystDirection_MuonIso", "nominal");
+    syst_muon_trk = ctx.get("SystDirection_MuonTrk", "nominal");
 
 
     //---------------------//
@@ -75,8 +79,12 @@ namespace uhh2 {
     //---------------//
 
     sf_lumi.reset(new MCLumiWeight(ctx));
-    sf_pileup.reset(new MCPileupReweight(ctx, syst_PU));
-    sf_toppt.reset(new TopPtReweight(ctx, 0.0615, -0.0005, "", "", bTopPtReweighting, 1.0)); // https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
+    sf_pileup.reset(new MCPileupReweight(ctx, syst_pileup));
+    sf_muon_trig.reset(new MCMuonScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/MuonTrigger_EfficienciesAndSF_average_RunBtoH.root", "IsoMu24_OR_IsoTkMu24_PtEtaBins", 0.5, "trigger", true, syst_muon_trigger));
+    sf_muon_id.reset(new MCMuonScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/MuonID_EfficienciesAndSF_average_RunBtoH.root", "MC_NUM_TightID_DEN_genTracks_PAR_pt_eta", 1, "tightID", true, syst_muon_id));
+    sf_muon_iso.reset(new MCMuonScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/MuonIso_EfficienciesAndSF_average_RunBtoH.root", "TightISO_TightID_pt_eta", 1, "iso", true, syst_muon_iso));
+    sf_muon_trk.reset(new MCMuonTrkScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/Tracking_EfficienciesAndSF_BCDEFGH.root", 1, "track", syst_muon_trk));
+    sf_toppt.reset(new TopPtReweight(ctx, 0.0615, -0.0005, "", "", true, 1.0)); // https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
     scale_variation.reset(new MCScaleVariation(ctx));
 
 
@@ -90,6 +98,7 @@ namespace uhh2 {
     //------------//
 
     hist_sf.reset(new AndHists(ctx, "0_ScaleFactors"));
+    hist_topptreweighting.reset(new AndHists(ctx, "1_TopPtReweighting"));
 
 
     //---------------//
@@ -110,19 +119,26 @@ namespace uhh2 {
       sf_lumi->process(event);
       sf_pileup->process(event);
       if(is_muon) {
-	// muon trigger, id, iso, tk
+	sf_muon_trig->process(event);
+	sf_muon_id->process(event);
+	sf_muon_iso->process(event);
+	sf_muon_trk->process(event);
       }
       else if(is_elec) {
 	// electron trigger, id, reco
       }
-      if(bTopPtReweighting && dataset_version.find("TTbar") == 0) sf_toppt->process(event);
+      //if(bTopPtReweighting && dataset_version.find("TTbar") == 0) sf_toppt->process(event);
       scale_variation->process(event);
     }
     hist_sf->fill(event);
 
+    // Apply top pt reweighting
+    if(dataset_version.find("TTbar") == 0) sf_toppt->process(event);
+    hist_topptreweighting->fill(event);
+
     // Place analysis routines into a new Module!!!
     // End of main selection
-    return true;
+    return false;
   }
 
 
