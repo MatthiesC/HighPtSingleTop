f990d9bb2e5319c7bf720bfbe5f3fbafeae12a00
Christopher Matthies <Christopher.Matthies@desy.de>
2019-04-30 11:49:51 +0200

    Comment out Top Pt Reweighting since not recommended to use for SM tW measurement

--- src/HighPtSingleTopMainSelectionModule.cxx
+++ src/HighPtSingleTopMainSelectionModule.cxx
@@ -23,10 +23,10 @@ namespace uhh2 {
     
   private:
     
-    std::unique_ptr<AnalysisModule> sf_lumi, sf_pileup, sf_muon_trig, sf_muon_id, sf_muon_iso, sf_muon_trk, sf_toppt;
+    std::unique_ptr<AnalysisModule> sf_lumi, sf_pileup, sf_muon_trig, sf_muon_id, sf_muon_iso, sf_muon_trk;//, sf_toppt;
     std::unique_ptr<AnalysisModule> scale_variation;
     
-    std::unique_ptr<AndHists> hist_noweights, hist_sf, hist_topptreweighting;
+    std::unique_ptr<AndHists> hist_noweights, hist_lumipuweights, hist_leptonsf;//, hist_topptreweighting;
 
     bool is_data, is_mc, is_muon, is_elec, bTopPtReweighting;
     string dataset_version;
@@ -84,7 +84,7 @@ namespace uhh2 {
     sf_muon_id.reset(new MCMuonScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/MuonID_EfficienciesAndSF_average_RunBtoH.root", "MC_NUM_TightID_DEN_genTracks_PAR_pt_eta", 1, "tightID", true, syst_muon_id));
     sf_muon_iso.reset(new MCMuonScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/MuonIso_EfficienciesAndSF_average_RunBtoH.root", "TightISO_TightID_pt_eta", 1, "iso", true, syst_muon_iso));
     sf_muon_trk.reset(new MCMuonTrkScaleFactor(ctx, "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/common/data/Tracking_EfficienciesAndSF_BCDEFGH.root", 1, "track", syst_muon_trk));
-    sf_toppt.reset(new TopPtReweight(ctx, 0.0615, -0.0005, "", "", true, 1.0)); // https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
+    //sf_toppt.reset(new TopPtReweight(ctx, 0.0615, -0.0005, "", "", true, 1.0)); // https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
     scale_variation.reset(new MCScaleVariation(ctx));
 
 
@@ -98,8 +98,9 @@ namespace uhh2 {
     //------------//
 
     hist_noweights.reset(new AndHists(ctx, "0_NoWeights")); // no weights except initial MC weights
-    hist_sf.reset(new AndHists(ctx, "1_ScaleFactors"));
-    hist_topptreweighting.reset(new AndHists(ctx, "2_TopPtReweighting"));
+    hist_lumipuweights.reset(new AndHists(ctx, "1_LumiAndPileupWeights"));
+    hist_leptonsf.reset(new AndHists(ctx, "2_LeptonScaleFactors"));
+    //hist_topptreweighting.reset(new AndHists(ctx, "3_TopPtReweighting"));
 
 
     //---------------//
@@ -115,13 +116,14 @@ namespace uhh2 {
 
   bool HighPtSingleTopMainSelectionModule::process(Event & event) {
 
-    // Histograms without weights
-    hist_noweights->fill(event);
-
     // Apply scale factors
+    hist_noweights->fill(event);
     if(is_mc) {
       sf_lumi->process(event);
       sf_pileup->process(event);
+    }
+    hist_lumipuweights->fill(event);
+    if(is_mc) {
       if(is_muon) {
 	sf_muon_trig->process(event);
 	sf_muon_id->process(event);
@@ -131,14 +133,15 @@ namespace uhh2 {
       else if(is_elec) {
 	// electron trigger, id, reco
       }
-      //if(bTopPtReweighting && dataset_version.find("TTbar") == 0) sf_toppt->process(event);
-      scale_variation->process(event);
     }
-    hist_sf->fill(event);
+    hist_leptonsf->fill(event);
+
+    // Scale variations
+    if(is_mc) scale_variation->process(event);
 
     // Apply top pt reweighting
-    if(dataset_version.find("TTbar") == 0) sf_toppt->process(event);
-    hist_topptreweighting->fill(event);
+    //if(dataset_version.find("TTbar") == 0) sf_toppt->process(event);
+    //hist_topptreweighting->fill(event);
 
     // Place analysis routines into a new Module!!!
     // End of main selection
