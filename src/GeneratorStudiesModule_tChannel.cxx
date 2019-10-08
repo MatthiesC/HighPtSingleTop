f9f60d5c56dd359f8279d1c7c668a6fa9d99f2a8
Christopher Matthies <Christopher.Matthies@desy.de>
2019-10-08 18:54:54 +0200

    Add module to run t-channel generator studies

---
+++ src/GeneratorStudiesModule_tChannel.cxx
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <memory>
+
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+
+#include "UHH2/common/include/MCWeight.h"
+
+#include "UHH2/HighPtSingleTop/include/SingleTopGen_tChannel.h"
+
+
+using namespace std;
+using namespace uhh2;
+
+namespace uhh2 {
+
+  class GeneratorStudiesModule_tChannel: public AnalysisModule {
+  public:
+    
+    explicit GeneratorStudiesModule_tChannel(Context & ctx);
+    virtual bool process(Event & event) override;
+    
+  private:
+
+    string dataset_version;
+    std::unique_ptr<AnalysisModule> sf_lumi;//, sf_pileup;
+    std::unique_ptr<AnalysisModule> SingleTopGen_tChannelProd;
+  };
+
+
+  GeneratorStudiesModule_tChannel::GeneratorStudiesModule_tChannel(Context & ctx){
+
+    dataset_version = ctx.get("dataset_version");
+
+    sf_lumi.reset(new MCLumiWeight(ctx));
+    //sf_pileup.reset(new MCPileupReweight(ctx,"nominal"));
+
+    SingleTopGen_tChannelProd.reset(new SingleTopGen_tChannelProducer(ctx, "h_GENtCh"));
+  }
+
+
+  bool GeneratorStudiesModule_tChannel::process(Event & event) {
+
+    sf_lumi->process(event);
+    //sf_pileup->process(event);
+
+    return true;
+  }
+
+
+  UHH2_REGISTER_ANALYSIS_MODULE(GeneratorStudiesModule_tChannel)
+}
