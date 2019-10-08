9a45e01d2949b614616339fb949162fd40591c3a
Christopher Matthies <Christopher.Matthies@desy.de>
2019-10-08 16:27:56 +0200

    Add first version of SingleTopGen for the t-channel

---
+++ include/SingleTopGen_tChannel.h
@@ -0,0 +1,91 @@
+// based on TTbarGen.h
+
+#pragma once
+
+#include "UHH2/core/include/GenParticle.h"
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+
+#include <vector>
+
+
+/** \brief ttbar generator truth information
+ * 
+ * Interpret a vector of GenParticle as ttbar event, providing easier access to
+ * the various components in the ttbar decay chain.
+ * 
+ * The class can either be used directly by passing the genparticles vector;
+ * another option is to use the SingleTopGen_tChannelProducer (see below), which writes the
+ * result to the event container, where it can be accessed later.
+ */
+class SingleTopGen_tChannel {
+public:
+  
+  /** construct from genparticles.
+   * 
+   * The event should be an actual ttbar event, i.e.:
+   *   - there are exactly two tops: one top and one anti-top
+   *   - each top must have exactl two daughters
+   *   - one of the top daughters must be a W
+   *   - the Ws must have exactly two daughters
+   * 
+   * Note that it is allowed that more particles than those belonging to the ttbar system
+   * are in genparts; those are ignored.
+   * 
+   * In case one of these conditions is not fulfilled, the behavior
+   * depends on the throw_on_failure parameter: if it is true (the default), a runtime_error
+   * is thrown with an explanation what failed. If it is false, no exception is thrown, but
+   * not all contents is valid; most will return a 0 vector. The one thing guaranteed is that the
+   * decaychannel will be e_notfound. If using throw_on_failure = false, it is thus a good idea
+   * to check the decaychannel.
+   */
+  explicit SingleTopGen_tChannel(const std::vector<GenParticle> & genparts, bool throw_on_failure = true);
+
+  enum E_DecayChannel{
+    e_tophad,
+    e_topele,
+    e_topmuo,
+    e_toptau,
+    e_notfound
+  };
+
+  GenParticle Top() const;
+  GenParticle WTop() const;
+  GenParticle bTop() const;
+  GenParticle WTopDecay1() const;
+  GenParticle WTopDecay2() const;
+  GenParticle Initial1() const;
+  GenParticle Initial2() const;
+  E_DecayChannel DecayChannel() const;
+
+  bool IsTopHadronicDecay() const;
+  bool IsTopLeptonicDecay() const;
+  bool IsTopToElectronDecay() const;
+  bool IsTopToMuonDecay() const;
+  bool IsTopToTauonDecay() const;
+
+ private:
+
+  GenParticle m_Top; 
+  GenParticle m_WTop; 
+  GenParticle m_bTop; 
+  GenParticle m_WTopDecay1; 
+  GenParticle m_WTopDecay2; 
+  GenParticle m_initial1;
+  GenParticle m_initial2;
+
+  E_DecayChannel m_type;
+};
+
+
+class SingleTopGen_tChannelProducer: public uhh2::AnalysisModule {
+public:
+    explicit SingleTopGen_tChannelProducer(uhh2::Context & ctx, const std::string & name = "singletopgen_tChannel", bool throw_on_failure = true);
+    virtual bool process(uhh2::Event & event) override;
+    
+private:
+    uhh2::Event::Handle<SingleTopGen_tChannel> h_singletopgen_tChannel;
+    bool throw_on_failure;
+};
+
+
---
+++ src/SingleTopGen_tChannel.cxx
@@ -0,0 +1,210 @@
+#include "UHH2/HighPtSingleTop/include/SingleTopGen_tChannel.h"
+
+using namespace std;
+using namespace uhh2;
+
+SingleTopGen_tChannel::SingleTopGen_tChannel(const vector<GenParticle> & genparticles, bool throw_on_failure): m_type(e_notfound) {    
+
+  int n_top = 0;
+  GenParticle top, initial1, initial2;
+
+  for(unsigned int i = 0; i < genparticles.size(); ++i) {
+    const GenParticle & genp = genparticles[i];
+    if (genp.index() == 0){ // find the initial state particles
+      initial1 = genp;
+      m_initial1 = initial1;
+    }
+    else if (genp.index() == 1){
+      initial2 = genp;
+      m_initial2 = initial2;
+    }
+    else if (abs(genp.pdgId()) == 6){ // We don't distinguish between top and antitop. May be changed later but for now it's okay
+      top = genp;
+      auto w = genp.daughter(&genparticles, 1);
+      auto b = genp.daughter(&genparticles, 2);
+      if(!w || !b) {
+        if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: top has not ==2 daughters");
+        return;
+      }
+      if (abs(w->pdgId()) != 24) {
+        std::swap(w, b);
+      }
+      /* It rarely happens that the list of genparticles contains 4 or more (instead of 2) particles which reckon the same top
+         as their mother although each particle including the tops can just have two daughters. E.g. if the top emits a photon
+         before decaying into b and W, this photon may split up into two leptons which reckon the top as their mother, too.
+         Therefore, it may happen that those leptons are considered as the top daughters whereas b and W are "ignored" and cannot
+         be found. This workaround fixes that issue: */
+      if(abs(w->pdgId()) != 24) {
+        for(unsigned int j = 0; j < genparticles.size(); ++j) {
+          const GenParticle & gp = genparticles[j];
+          auto m1 = gp.mother(&genparticles, 1);
+          auto m2 = gp.mother(&genparticles, 2);
+          bool has_top_mother = ((m1 && m1->index() == genp.index()) || (m2 && m2->index() == genp.index()));
+          if(has_top_mother && (abs(gp.pdgId()) == 24)) {
+            w = &gp;
+            break;
+          }
+        }
+      }
+      if (abs(w->pdgId()) != 24) {
+        if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: top has no W daughter");
+        return;
+      }
+
+      /* Do a similar workaround as above if the expected b daughter has not been found yet */
+      if(abs(b->pdgId()) != 5 && abs(b->pdgId()) != 3 && abs(b->pdgId()) != 1) {
+        for(unsigned int j = 0; j < genparticles.size(); ++j) {
+          const GenParticle & gp = genparticles[j];
+          auto m1 = gp.mother(&genparticles, 1);
+          auto m2 = gp.mother(&genparticles, 2);
+          bool has_top_mother = ((m1 && m1->index() == genp.index()) || (m2 && m2->index() == genp.index()));
+          if(has_top_mother && (abs(gp.pdgId()) == 5 || abs(gp.pdgId()) == 3 || abs(gp.pdgId()) == 1)) {
+            b = &gp;
+            break;
+          }
+        }
+      }
+      if (abs(b->pdgId()) != 5 && abs(b->pdgId()) != 3   && abs(b->pdgId()) != 1) {
+        if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: top has no b daughter");
+        return;
+      }
+      // now get WTop daughters:
+
+      int n_WTopDau = 0;
+
+      auto wd1 = w->daughter(&genparticles, 1);
+      auto wd2 = w->daughter(&genparticles, 2);
+
+      while (n_WTopDau != 2){
+
+        if(wd1 && !wd2){
+          w = wd1;
+          wd1 = w->daughter(&genparticles, 1);
+          wd2 = w->daughter(&genparticles, 2);
+        }
+        else if(wd1 && wd2){
+          n_WTopDau = 2;
+        }
+
+        else{
+          if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: WTop has no daughters");
+          return;
+        }
+
+      }
+      if(!wd1 || !wd2){
+        if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: WTop has not ==2 daughters");
+        return;
+      }
+            
+      // now that we collected everything, fill the member variables. 
+      // Unlike in TTbarGen.cxx, where we use different member variables according to top quark charge, we do not distinguish them here because there is just one top
+      m_Top = top;
+      m_WTop = *w;
+      m_bTop = *b;
+      m_WTopDecay1 = *wd1;
+      m_WTopDecay2 = *wd2;
+      ++n_top;
+
+    }
+    
+  }
+
+
+  if(n_top != 1){
+    if(throw_on_failure) throw runtime_error("SingleTopGen_tChannel: did not find exactly one (anti)top in the event");
+    return;
+  }
+  
+  
+  // calculate decay channel by counting the number of charged leptons in the WTop daughters
+  int n_e = 0, n_m = 0, n_t = 0;
+  for(const auto & wd : {m_WTopDecay1, m_WTopDecay2}){
+    int id_wd = abs(wd.pdgId());
+    if(id_wd == 11) ++n_e;
+    else if(id_wd == 13) ++n_m;
+    else if(id_wd == 15) ++n_t;
+  }
+  if(n_e == 1){
+    m_type = e_topele;
+  }
+  else if(n_m == 1){
+    m_type = e_topmuo;
+  }
+  else if(n_t == 1){
+    m_type = e_toptau;
+  }
+  else if(n_e + n_m + n_t == 0){
+    m_type = e_tophad;
+  }
+  else{ // this should not happen except when the W has more than one charged lepton under its daughters (maybe because of W --> W gamma --> 3 lep + 1 nu)
+    m_type = e_notfound;
+  }
+
+}
+
+
+GenParticle SingleTopGen_tChannel::Top() const{
+  return m_Top;
+}
+
+GenParticle SingleTopGen_tChannel::WTop() const{
+  return m_WTop;
+}
+
+GenParticle SingleTopGen_tChannel::bTop() const{
+  return m_bTop;
+}
+
+GenParticle SingleTopGen_tChannel::WTopDecay1() const{
+  return m_WTopDecay1;
+} 
+
+GenParticle SingleTopGen_tChannel::WTopDecay2() const{
+  return m_WTopDecay2;
+} 
+
+GenParticle SingleTopGen_tChannel::Initial1() const{
+  return m_initial1;
+}
+
+GenParticle SingleTopGen_tChannel::Initial2() const{
+  return m_initial2;
+}
+
+
+SingleTopGen_tChannel::E_DecayChannel SingleTopGen_tChannel::DecayChannel() const{  
+  return m_type;
+}
+
+
+bool SingleTopGen_tChannel::IsTopHadronicDecay() const{
+  return m_type == e_tophad;
+}
+
+bool SingleTopGen_tChannel::IsTopLeptonicDecay() const{
+  return m_type == e_topele || m_type == e_topmuo || m_type == e_toptau;
+}
+
+bool SingleTopGen_tChannel::IsTopToElectronDecay() const{
+  return m_type == e_topele;
+}
+
+bool SingleTopGen_tChannel::IsTopToMuonDecay() const{
+  return m_type == e_topmuo;
+}
+
+bool SingleTopGen_tChannel::IsTopToTauonDecay() const{
+  return m_type == e_toptau;
+}
+
+
+
+SingleTopGen_tChannelProducer::SingleTopGen_tChannelProducer(uhh2::Context & ctx, const std::string & name, bool throw_on_failure_): throw_on_failure(throw_on_failure_){
+  h_singletopgen_tChannel = ctx.get_handle<SingleTopGen_tChannel>(name);
+}
+
+bool SingleTopGen_tChannelProducer::process(Event & event){
+  event.set(h_singletopgen_tChannel, SingleTopGen_tChannel(*event.genparticles, throw_on_failure));
+  return true;
+}
