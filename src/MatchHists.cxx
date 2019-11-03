6131dccd09875147587b2bfbcfd392de492ee104
Christopher Matthies <Christopher.Matthies@desy.de>
2019-11-03 01:58:35 +0100

    Use predefined nBin integers && do not try to re-label x-axis with PI && remove n/a bin from decay channel histogram

--- src/MatchHists.cxx
+++ src/MatchHists.cxx
@@ -15,36 +15,43 @@ MatchHists::MatchHists(Context & ctx, const string & dirname):
   h_toptaggedjet = ctx.get_handle<TopJet>("TopTaggedJet");
   h_primlep = ctx.get_handle<FlavorParticle>("PrimaryLepton");
 
-  hist_deltaR_topQuark_topRecoJet = book<TH1F>("deltaR_topQuark_topRecoJet", "#DeltaR(gen. t quark, HOTVR t-tag)", 100, 0, 5);
-  hist_deltaPhi_topQuark_topRecoJet = book<TH1F>("deltaPhi_topQuark_topRecoJet", "#Delta#phi(gen. t quark, HOTVR t-tag)", 128, 0, M_PI);
-  hist_deltaR_topQuark_recoLepton = book<TH1F>("deltaR_topQuark_recoLepton", "#DeltaR(gen. t quark, rec. lepton)", 100, 0, 5);
-  hist_deltaPhi_topQuark_recoLepton = book<TH1F>("deltaPhi_topQuark_recoLepton", "#Delta#phi(gen. t quark, rec. lepton)", 128, 0, M_PI);
+  const int nBins_dR = 100;
+  const int nBins_dPhi = 128;
+  const int nBins_dR_lowRes = 50;
+  const int nBins_dPhi_lowRes = 64;
+  
 
-  hist_deltaR_WBoson_topRecoJet = book<TH1F>("deltaR_WBoson_topRecoJet", "#DeltaR(gen. assoc. W, HOTVR t-tag)", 100, 0, 5);
-  hist_deltaPhi_WBoson_topRecoJet = book<TH1F>("deltaPhi_WBoson_topRecoJet", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", 128, 0, M_PI);
-  hist_deltaR_WBoson_recoLepton = book<TH1F>("deltaR_WBoson_recoLepton", "#DeltaR(gen. assoc. W, rec. lepton)", 100, 0, 5);
-  hist_deltaPhi_WBoson_recoLepton = book<TH1F>("deltaPhi_WBoson_recoLepton", "#Delta#phi(gen. assoc. W, rec. lepton)", 128, 0, M_PI);
+  hist_deltaR_topQuark_topRecoJet = book<TH1F>("deltaR_topQuark_topRecoJet", "#DeltaR(gen. t quark, HOTVR t-tag)", nBins_dR, 0, 5);
+  hist_deltaPhi_topQuark_topRecoJet = book<TH1F>("deltaPhi_topQuark_topRecoJet", "#Delta#phi(gen. t quark, HOTVR t-tag)", nBins_dPhi, 0, M_PI);
+  hist_deltaR_topQuark_recoLepton = book<TH1F>("deltaR_topQuark_recoLepton", "#DeltaR(gen. t quark, rec. lepton)", nBins_dR, 0, 5);
+  hist_deltaPhi_topQuark_recoLepton = book<TH1F>("deltaPhi_topQuark_recoLepton", "#Delta#phi(gen. t quark, rec. lepton)", nBins_dPhi, 0, M_PI);
 
-  hist_deltaR_topQuark_topRecoJet_Pt0to400 = book<TH1F>("deltaR_topQuark_topRecoJet_Pt0to400", "#DeltaR(gen. t quark, HOTVR t-tag)", 100, 0, 5);
-  hist_deltaPhi_topQuark_topRecoJet_Pt0to400 = book<TH1F>("deltaPhi_topQuark_topRecoJet_Pt0to400", "#Delta#phi(gen. t quark, HOTVR t-tag)", 128, 0, M_PI);
-  hist_deltaR_topQuark_recoLepton_Pt0to400 = book<TH1F>("deltaR_topQuark_recoLepton_Pt0to400", "#DeltaR(gen. t quark, rec. lepton)", 100, 0, 5);
-  hist_deltaPhi_topQuark_recoLepton_Pt0to400 = book<TH1F>("deltaPhi_topQuark_recoLepton_Pt0to400", "#Delta#phi(gen. t quark, rec. lepton)", 128, 0, M_PI);
+  hist_deltaR_WBoson_topRecoJet = book<TH1F>("deltaR_WBoson_topRecoJet", "#DeltaR(gen. assoc. W, HOTVR t-tag)", nBins_dR, 0, 5);
+  hist_deltaPhi_WBoson_topRecoJet = book<TH1F>("deltaPhi_WBoson_topRecoJet", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", nBins_dPhi, 0, M_PI);
+  hist_deltaR_WBoson_recoLepton = book<TH1F>("deltaR_WBoson_recoLepton", "#DeltaR(gen. assoc. W, rec. lepton)", nBins_dR, 0, 5);
+  hist_deltaPhi_WBoson_recoLepton = book<TH1F>("deltaPhi_WBoson_recoLepton", "#Delta#phi(gen. assoc. W, rec. lepton)", nBins_dPhi, 0, M_PI);
 
-  hist_deltaR_WBoson_topRecoJet_Pt0to400 = book<TH1F>("deltaR_WBoson_topRecoJet_Pt0to400", "#DeltaR(gen. assoc. W, HOTVR t-tag)", 100, 0, 5);
-  hist_deltaPhi_WBoson_topRecoJet_Pt0to400 = book<TH1F>("deltaPhi_WBoson_topRecoJet_Pt0to400", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", 128, 0, M_PI);
-  hist_deltaR_WBoson_recoLepton_Pt0to400 = book<TH1F>("deltaR_WBoson_recoLepton_Pt0to400", "#DeltaR(gen. assoc. W, rec. lepton)", 100, 0, 5);
-  hist_deltaPhi_WBoson_recoLepton_Pt0to400 = book<TH1F>("deltaPhi_WBoson_recoLepton_Pt0to400", "#Delta#phi(gen. assoc. W, rec. lepton)", 128, 0, M_PI);
+  hist_deltaR_topQuark_topRecoJet_Pt0to400 = book<TH1F>("deltaR_topQuark_topRecoJet_Pt0to400", "#DeltaR(gen. t quark, HOTVR t-tag)", nBins_dR, 0, 5);
+  hist_deltaPhi_topQuark_topRecoJet_Pt0to400 = book<TH1F>("deltaPhi_topQuark_topRecoJet_Pt0to400", "#Delta#phi(gen. t quark, HOTVR t-tag)", nBins_dPhi, 0, M_PI);
+  hist_deltaR_topQuark_recoLepton_Pt0to400 = book<TH1F>("deltaR_topQuark_recoLepton_Pt0to400", "#DeltaR(gen. t quark, rec. lepton)", nBins_dR, 0, 5);
+  hist_deltaPhi_topQuark_recoLepton_Pt0to400 = book<TH1F>("deltaPhi_topQuark_recoLepton_Pt0to400", "#Delta#phi(gen. t quark, rec. lepton)", nBins_dPhi, 0, M_PI);
 
-  hist_deltaR_topQuark_topRecoJet_Pt400toInf = book<TH1F>("deltaR_topQuark_topRecoJet_Pt400toInf", "#DeltaR(gen. t quark, HOTVR t-tag)", 20, 0, 5);
-  hist_deltaPhi_topQuark_topRecoJet_Pt400toInf = book<TH1F>("deltaPhi_topQuark_topRecoJet_Pt400toInf", "#Delta#phi(gen. t quark, HOTVR t-tag)", 16, 0, M_PI);
-  hist_deltaR_topQuark_recoLepton_Pt400toInf = book<TH1F>("deltaR_topQuark_recoLepton_Pt400toInf", "#DeltaR(gen. t quark, rec. lepton)", 20, 0, 5);
-  hist_deltaPhi_topQuark_recoLepton_Pt400toInf = book<TH1F>("deltaPhi_topQuark_recoLepton_Pt400toInf", "#Delta#phi(gen. t quark, rec. lepton)", 16, 0, M_PI);
+  hist_deltaR_WBoson_topRecoJet_Pt0to400 = book<TH1F>("deltaR_WBoson_topRecoJet_Pt0to400", "#DeltaR(gen. assoc. W, HOTVR t-tag)", nBins_dR, 0, 5);
+  hist_deltaPhi_WBoson_topRecoJet_Pt0to400 = book<TH1F>("deltaPhi_WBoson_topRecoJet_Pt0to400", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", nBins_dPhi, 0, M_PI);
+  hist_deltaR_WBoson_recoLepton_Pt0to400 = book<TH1F>("deltaR_WBoson_recoLepton_Pt0to400", "#DeltaR(gen. assoc. W, rec. lepton)", nBins_dR, 0, 5);
+  hist_deltaPhi_WBoson_recoLepton_Pt0to400 = book<TH1F>("deltaPhi_WBoson_recoLepton_Pt0to400", "#Delta#phi(gen. assoc. W, rec. lepton)", nBins_dPhi, 0, M_PI);
 
-  hist_deltaR_WBoson_topRecoJet_Pt400toInf = book<TH1F>("deltaR_WBoson_topRecoJet_Pt400toInf", "#DeltaR(gen. assoc. W, HOTVR t-tag)", 20, 0, 5);
-  hist_deltaPhi_WBoson_topRecoJet_Pt400toInf = book<TH1F>("deltaPhi_WBoson_topRecoJet_Pt400toInf", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", 16, 0, M_PI);
-  hist_deltaR_WBoson_recoLepton_Pt400toInf = book<TH1F>("deltaR_WBoson_recoLepton_Pt400toInf", "#DeltaR(gen. assoc. W, rec. lepton)", 20, 0, 5);
-  hist_deltaPhi_WBoson_recoLepton_Pt400toInf = book<TH1F>("deltaPhi_WBoson_recoLepton_Pt400toInf", "#Delta#phi(gen. assoc. W, rec. lepton)", 16, 0, M_PI);
+  hist_deltaR_topQuark_topRecoJet_Pt400toInf = book<TH1F>("deltaR_topQuark_topRecoJet_Pt400toInf", "#DeltaR(gen. t quark, HOTVR t-tag)", nBins_dR_lowRes, 0, 5);
+  hist_deltaPhi_topQuark_topRecoJet_Pt400toInf = book<TH1F>("deltaPhi_topQuark_topRecoJet_Pt400toInf", "#Delta#phi(gen. t quark, HOTVR t-tag)", nBins_dPhi_lowRes, 0, M_PI);
+  hist_deltaR_topQuark_recoLepton_Pt400toInf = book<TH1F>("deltaR_topQuark_recoLepton_Pt400toInf", "#DeltaR(gen. t quark, rec. lepton)", nBins_dR_lowRes, 0, 5);
+  hist_deltaPhi_topQuark_recoLepton_Pt400toInf = book<TH1F>("deltaPhi_topQuark_recoLepton_Pt400toInf", "#Delta#phi(gen. t quark, rec. lepton)", nBins_dPhi_lowRes, 0, M_PI);
 
+  hist_deltaR_WBoson_topRecoJet_Pt400toInf = book<TH1F>("deltaR_WBoson_topRecoJet_Pt400toInf", "#DeltaR(gen. assoc. W, HOTVR t-tag)", nBins_dR_lowRes, 0, 5);
+  hist_deltaPhi_WBoson_topRecoJet_Pt400toInf = book<TH1F>("deltaPhi_WBoson_topRecoJet_Pt400toInf", "#Delta#phi(gen. assoc. W, HOTVR t-tag)", nBins_dPhi_lowRes, 0, M_PI);
+  hist_deltaR_WBoson_recoLepton_Pt400toInf = book<TH1F>("deltaR_WBoson_recoLepton_Pt400toInf", "#DeltaR(gen. assoc. W, rec. lepton)", nBins_dR_lowRes, 0, 5);
+  hist_deltaPhi_WBoson_recoLepton_Pt400toInf = book<TH1F>("deltaPhi_WBoson_recoLepton_Pt400toInf", "#Delta#phi(gen. assoc. W, rec. lepton)", nBins_dPhi_lowRes, 0, M_PI);
+
+  /* // This does not work... maybe implement pi Xaxis in SFramePlotter...
   for(auto hist : {hist_deltaPhi_topQuark_topRecoJet, hist_deltaPhi_topQuark_topRecoJet, hist_deltaPhi_WBoson_topRecoJet, hist_deltaPhi_WBoson_recoLepton,
         hist_deltaPhi_topQuark_topRecoJet_Pt0to400, hist_deltaPhi_topQuark_topRecoJet_Pt0to400, hist_deltaPhi_WBoson_topRecoJet_Pt0to400, hist_deltaPhi_WBoson_recoLepton_Pt0to400,
         hist_deltaPhi_topQuark_topRecoJet_Pt400toInf, hist_deltaPhi_topQuark_topRecoJet_Pt400toInf, hist_deltaPhi_WBoson_topRecoJet_Pt400toInf, hist_deltaPhi_WBoson_recoLepton_Pt400toInf}) {
@@ -53,14 +60,14 @@ MatchHists::MatchHists(Context & ctx, const string & dirname):
     hist->GetXaxis()->ChangeLabel(2,-1,-1,-1,-1,-1,"#pi/2");
     hist->GetXaxis()->ChangeLabel(-1,-1,-1,-1,-1,-1,"#pi");
   }
+  */
 
-  const int nx = 17;
-  hist_decayChannel = book<TH1F>("DecayChannel", "tW decay channel", nx, -0.5, 16.5);
+  const int nx = 16;
+  hist_decayChannel = book<TH1F>("DecayChannel", "tW decay channel", nx, -0.5, 15.5);
   const char *decayChannels[nx] = {"t_{e}W_{e}", "t_{#mu}W_{e}", "t_{#tau}W_{e}", "t_{h}W_{e}",
                                    "t_{e}W_{#mu}", "t_{#mu}W_{#mu}", "t_{#tau}W_{#mu}", "t_{h}W_{#mu}",
                                    "t_{e}W_{#tau}", "t_{#mu}W_{#tau}", "t_{#tau}W_{#tau}", "t_{h}W_{#tau}",
-				   "t_{e}W_{h}", "t_{#mu}W_{h}", "t_{#tau}W_{h}", "t_{h}W_{h}",
-                                   "N/A"};
+				   "t_{e}W_{h}", "t_{#mu}W_{h}", "t_{#tau}W_{h}", "t_{h}W_{h}"};
   for(int i=1; i<=nx; ++i) hist_decayChannel->GetXaxis()->SetBinLabel(i,decayChannels[i-1]);
 }
 
