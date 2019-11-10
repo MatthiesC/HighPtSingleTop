c3d16c9dfadf47d42ae78f6be2ac6c1b823f3f1f
Christopher Matthies <Christopher.Matthies@desy.de>
2019-11-10 19:23:09 +0100

    New plotting steer files and script to plot everyhting

---
+++ sframeplotter/plot_all_ttbarScaled.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Plots -f MainSel2016MuoLin_ttbarScaled.steer
+Plots -f MainSel2016MuoLinSingleEPS_ttbarScaled.steer
+Plots -f MainSel2016MuoLinNorm.steer
+Plots -f MainSel2016MuoLinNormSingleEPS.steer
+Plots -f MatchHists2016MuoLin.steer
+Plots -f MatchHists2016MuoLinSingleEPS.steer
