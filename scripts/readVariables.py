c0bbe2f6776cb2a3baaf9876972b84ac26afd34d
Christopher Matthies <Christopher.Matthies@desy.de>
2020-01-25 02:32:04 +0100

    Add script to read variables from parameters.json file for lwtnn

---
+++ scripts/readVariables.py
@@ -0,0 +1,18 @@
+import json
+
+with open('parameters.json') as f:
+    param = json.load(f)
+
+variables = param.get('inputVars')
+
+inputs = list()
+for v in variables:
+    inputs.append({'name': str(v[0]), 'offset': -float(v[1]), 'scale': float(v[2])})
+
+result = {
+    'inputs': inputs,
+    'class_labels': ['BinaryClassificationOutputName']
+}
+
+with open('variables.json', 'w') as outfile:
+    json.dump(result, outfile)
