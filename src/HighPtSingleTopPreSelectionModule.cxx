27d3f36d9e1c4a69bad54bdcc043b39fa9b57a96
Christopher Matthies <Christopher.Matthies@desy.de>
2020-02-14 18:18:36 +0100

    AK8-compatible preselection

--- src/HighPtSingleTopPreSelectionModule.cxx
+++ src/HighPtSingleTopPreSelectionModule.cxx
@@ -35,36 +35,30 @@ namespace uhh2 {
 
   class HighPtSingleTopPreSelectionModule: public AnalysisModule {
   public:
-    
+
     explicit HighPtSingleTopPreSelectionModule(Context & ctx);
     virtual bool process(Event & event) override;
-    
+
   private:
 
     Year year;
-    
-    std::unique_ptr<CommonModules> common_modules;
 
-    std::unique_ptr<AnalysisModule> clnr_muon, clnr_elec, clnr_hotvr, hotvr_jec_module, clnr_jetLeptonOverlap;
-    std::unique_ptr<AnalysisModule> primarylep;
-    std::unique_ptr<AnalysisModule> SingleTopGen_tWchProd;
-    std::unique_ptr<AnalysisModule> met_xy_correction;
-    
-    std::unique_ptr<Selection> slct_mttbarGenCut, slct_tWgenSignal;
-    std::unique_ptr<Selection> slct_lumi;//, slct_trigger;
-    std::unique_ptr<Selection> slct_1muon, slct_0muon, slct_1elec, slct_0elec;
-    std::unique_ptr<Selection> slct_met, slct_1jet, slct_1hotvr;
-    
-    std::unique_ptr<AndHists> hist_common, hist_trigger, hist_cleaning, hist_1lepton, hist_met, hist_1jet, hist_1hotvr;
-    //std::unique_ptr<Hists> hist_met_xy_uncorr, hist_met_xy_corr;
+    unique_ptr<CommonModules> common_modules;
 
-    bool is_data, is_mc, is_muon, is_elec;
+    unique_ptr<AnalysisModule> clnr_muon, clnr_elec, clnr_topjet, hotvr_jec_module, clnr_jetLeptonOverlap;
+    unique_ptr<AnalysisModule> primarylep;
+    unique_ptr<AnalysisModule> SingleTopGen_tWchProd;
+    unique_ptr<AnalysisModule> met_xy_correction;
+
+    unique_ptr<Selection> slct_mttbarGenCut, slct_tWgenSignal;
+    unique_ptr<Selection> slct_lumi;
+    unique_ptr<Selection> slct_1muon, slct_0muon, slct_1elec, slct_0elec;
+    unique_ptr<Selection> slct_met, slct_1topjet;//, slct_1jet,;
+
+    unique_ptr<AndHists> hist_common, hist_trigger, hist_cleaning, hist_1lepton, hist_met, hist_1topjet;//, hist_1jet;
+
+    bool is_data, is_mc, is_muon, is_elec, using_hotvr;
     string dataset_version, met_name, jet_collection;
-
-    double muonPt_min, muonEta_max, muonPt_min_veto, muonEta_max_veto, muonIso_max;
-    double elecPt_min, elecEta_max, elecPt_min_veto, elecEta_max_veto;
-    double met_min;
-    double jetPt_min, jetEta_max, hotvrPt_min, hotvrEta_max, hotvrDeltaRToLepton_min, jetDeltaRToLepton_min;
   };
 
 
@@ -87,35 +81,40 @@ namespace uhh2 {
     dataset_version = ctx.get("dataset_version");
     met_name = ctx.get("METName");
     jet_collection = ctx.get("JetCollection");
+    using_hotvr = ctx.get("TopJetCollection") == "hotvrPuppi"; // if false, we use AK8 Puppi
 
 
     //---------------------//
     // KINEMATIC VARIABLES //
     //---------------------//
 
-    muonPt_min_veto = 30.0;
-    muonEta_max_veto = 2.4;
+    double muonPt_min_veto = 30.0;
+    double muonEta_max_veto = 2.4;
 
-    elecPt_min_veto = 30.0;
-    elecEta_max_veto = 2.4;
+    double elecPt_min_veto = 30.0;
+    double elecEta_max_veto = 2.4;
 
-    jetPt_min = 30.0;
-    jetEta_max = 2.4;
+    double jetPt_min = 30.0;
+    double jetEta_max = 2.4;
 
-    hotvrPt_min = 200.0; // needs to be zero so that migrations from < 200 GeV bins into the boosted sector can still be calculated once the unfolding happens (although these migrations are probably tiny)
-    hotvrEta_max = 2.5;
+    double hotvrPt_min = 200.0;
+    double hotvrEta_max = 2.5;
 
-    hotvrDeltaRToLepton_min = 1.5;
-    jetDeltaRToLepton_min = 0.4;
+    double ak8Pt_min = 300.0;
+    double ak8Eta_max = 2.4;
 
-    muonPt_min  = 50.0;
-    muonEta_max =  2.4;
-    muonIso_max =  0.15;
+    double hotvrDeltaRToLepton_min = 1.5;
+    double jetDeltaRToLepton_min = 0.4;
+    double ak8DeltaRToLepton_min = 0.8;
 
-    elecPt_min  = 50.0;
-    elecEta_max =  2.4;
+    double muonPt_min  = 50.0;
+    double muonEta_max =  2.4;
+    double muonIso_max =  0.15;
 
-    met_min = 50.0;
+    double elecPt_min  = 50.0;
+    double elecEta_max =  2.4;
+
+    double met_min = 50.0;
 
 
     //-----------------//
@@ -128,20 +127,21 @@ namespace uhh2 {
     ElectronId elecID;
     if (year == Year::is2016v2 || year == Year::is2016v3)
       {
-	elecID_veto = AndId<Electron>(ElectronID_Summer16_veto, PtEtaCut(elecPt_min_veto, elecEta_max_veto));
-	elecID = AndId<Electron>(ElectronID_Summer16_tight, PtEtaCut(elecPt_min, elecEta_max));
+        elecID_veto = AndId<Electron>(ElectronID_Summer16_veto, PtEtaCut(elecPt_min_veto, elecEta_max_veto));
+        elecID = AndId<Electron>(ElectronID_Summer16_tight, PtEtaCut(elecPt_min, elecEta_max));
       }
     else
       {
-	elecID_veto = AndId<Electron>(ElectronID_Fall17_veto, PtEtaCut(elecPt_min_veto, elecEta_max_veto));
-	elecID = AndId<Electron>(ElectronID_Fall17_tight, PtEtaCut(elecPt_min, elecEta_max));
+        elecID_veto = AndId<Electron>(ElectronID_Fall17_veto, PtEtaCut(elecPt_min_veto, elecEta_max_veto));
+        elecID = AndId<Electron>(ElectronID_Fall17_tight, PtEtaCut(elecPt_min, elecEta_max));
       }
     JetPFID::wp jetPFID;
     if(jet_collection == "jetsAk4CHS") jetPFID = JetPFID::WP_TIGHT_CHS;
     else if(jet_collection == "jetsAk4Puppi") jetPFID = JetPFID::WP_TIGHT_PUPPI;
     else throw runtime_error("HighPtSingleTopPreSelectionModule: Please check jet PF ID");
     JetId jetID = PtEtaCut(jetPt_min, jetEta_max);
-    TopJetId hotvrID = AndId<TopJet>(PtEtaCut(hotvrPt_min, hotvrEta_max), DeltaRCut(ctx, hotvrDeltaRToLepton_min)); // // through away all HOTVR jets with lepton close by <-- Roman's recommendation from October 16, 2019
+    TopJetId hotvrID = AndId<TopJet>(PtEtaCut(hotvrPt_min, hotvrEta_max), DeltaRCut(ctx, hotvrDeltaRToLepton_min)); // through away all HOTVR jets with lepton close by <-- Roman's recommendation from October 16, 2019
+    TopJetId ak8ID = AndId<TopJet>(PtEtaCut(ak8Pt_min, ak8Eta_max), DeltaRCut(ctx, ak8DeltaRToLepton_min));
 
 
     //----------------//
@@ -162,7 +162,7 @@ namespace uhh2 {
 
     clnr_muon.reset(new MuonCleaner(muonID));
     clnr_elec.reset(new ElectronCleaner(elecID));
-    clnr_hotvr.reset(new TopJetCleaner(ctx, hotvrID));
+    clnr_topjet.reset(new TopJetCleaner(ctx, (using_hotvr ? hotvrID : ak8ID)));
     clnr_jetLeptonOverlap.reset(new JetLeptonOverlapRemoval(ctx, jetDeltaRToLepton_min));
 
 
@@ -175,35 +175,24 @@ namespace uhh2 {
     slct_mttbarGenCut.reset(new MttbarGenSelection(0, 700));
     slct_tWgenSignal.reset(new tWgenSignalSelection(ctx, is_muon));
 
-    // For recommendations, see https://twiki.cern.ch/twiki/bin/view/CMS/SingleTopTWRun2
-    // https://twiki.cern.ch/twiki/bin/view/CMS/MuonHLT2016
-    // https://twiki.cern.ch/twiki/bin/view/CMS/EgHLTRunIISummary
-    // https://indico.cern.ch/event/662745/   Approval of SFs for one of the previously used trigger combinations (see git commit history)
-    //slct_trigger.reset(new HighPtSingleTopTriggerSelection(ctx));
-
     slct_1muon.reset(new NMuonSelection(1, 1));
     slct_0muon.reset(new NMuonSelection(0, 0));
     slct_1elec.reset(new NElectronSelection(1, 1));
     slct_0elec.reset(new NElectronSelection(0, 0));
     slct_met.reset(new METSelection(met_min));
-    slct_1jet.reset(new NJetSelection(1, -1));
-    slct_1hotvr.reset(new NTopJetSelection(1, -1));
+    //slct_1jet.reset(new NJetSelection(1, -1));
+    slct_1topjet.reset(new NTopJetSelection(1, -1));
 
 
     //------------//
     // HISTOGRAMS //
     //------------//
 
-    //hist_met_xy_uncorr.reset(new MyEventHists(ctx, "00_MET_uncorr"));
-    //hist_met_xy_corr.reset(new MyEventHists(ctx, "01_MET_corr"));
-
-
     hist_common.reset(new AndHists(ctx, "0_Common"));
-    //hist_trigger.reset(new AndHists(ctx, "1_Trigger"));
-    hist_1lepton.reset(new AndHists(ctx, "2_OneLepton"));
-    hist_met.reset(new AndHists(ctx, "3_MET"));
-    hist_1jet.reset(new AndHists(ctx, "4_OneJet"));
-    hist_1hotvr.reset(new AndHists(ctx, "5_OneHotvr"));
+    hist_1lepton.reset(new AndHists(ctx, "1_OneLepton"));
+    hist_met.reset(new AndHists(ctx, "2_MET"));
+    //hist_1jet.reset(new AndHists(ctx, "4_OneJet"));
+    hist_1topjet.reset(new AndHists(ctx, "3_OneTopJet"));
 
 
     //---------------//
@@ -221,12 +210,12 @@ namespace uhh2 {
   //------------//
 
   bool HighPtSingleTopPreSelectionModule::process(Event & event) {
-    
+
     /* No event weights will be applied in this Module!!! Except for (via CommonModules):
        - MC lumi weight
        - MC pileup reweight
        I.e. the histograms filled here have little to no physical meaning! Be aware of that!
-     */
+    */
 
     // Lumi selection
     if(is_data && !slct_lumi->passes(event)) return false;
@@ -243,31 +232,25 @@ namespace uhh2 {
     if(dataset_version.find("TTbar_M700toInf") == 0 && slct_mttbarGenCut->passes(event)) return false;
 
     // Initial cleaning, MET+PV filter, and lumi+PU weights
-    //hist_met_xy_uncorr->fill(event);
-    //hist_met_xy_corr->fill(event);
-
     if(!common_modules->process(event)) return false;
     hist_common->fill(event);
     if(met_name == "slimmedMETs") met_xy_correction->process(event); // only apply XY corrections if using PF MET
 
-    // Trigger paths
-    //if(!slct_trigger->passes(event)) return false;
-    //hist_trigger->fill(event);
-
     // Single-lepton selection and veto on additional leptons
     if(is_muon)
       {
-	if(!(slct_1muon->passes(event) && slct_0elec->passes(event))) return false;
-	clnr_muon->process(event);
-	if(!slct_1muon->passes(event)) return false;
+        if(!(slct_1muon->passes(event) && slct_0elec->passes(event))) return false;
+        clnr_muon->process(event);
+        if(!slct_1muon->passes(event)) return false;
       }
     else if(is_elec)
       {
-	if(!(slct_1elec->passes(event) && slct_0muon->passes(event))) return false;
-	clnr_elec->process(event);
-	if(!slct_1elec->passes(event)) return false;
+        if(!(slct_1elec->passes(event) && slct_0muon->passes(event))) return false;
+        clnr_elec->process(event);
+        if(!slct_1elec->passes(event)) return false;
       }
     primarylep->process(event);
+    clnr_jetLeptonOverlap->process(event);
     hist_1lepton->fill(event);
 
     // MET selection
@@ -275,15 +258,14 @@ namespace uhh2 {
     hist_met->fill(event);
 
     // At least one AK4 jet
-    clnr_jetLeptonOverlap->process(event);
-    if(!slct_1jet->passes(event)) return false;
-    hist_1jet->fill(event);
+    //if(!slct_1jet->passes(event)) return false;
+    //hist_1jet->fill(event);
 
-    // At least one HOTVR jet
-    hotvr_jec_module->process(event);
-    clnr_hotvr->process(event);
-    if(!slct_1hotvr->passes(event)) return false;
-    hist_1hotvr->fill(event);
+    // At least one TOP jet
+    if(using_hotvr) hotvr_jec_module->process(event);
+    clnr_topjet->process(event);
+    if(!slct_1topjet->passes(event)) return false;
+    hist_1topjet->fill(event);
 
     // Place additional selections into a new Module!!!
     // End of preselection
