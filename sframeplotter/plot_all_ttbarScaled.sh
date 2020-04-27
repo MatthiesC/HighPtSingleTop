f5074fd2b2c4b60ec089e9b9052f1cd7b58f5893
Christopher Matthies <Christopher.Matthies@desy.de>
2020-04-27 07:01:05 +0200

    Added DS plots

--- sframeplotter/plot_all_ttbarScaled.sh
+++ sframeplotter/plot_all_ttbarScaled.sh
@@ -3,9 +3,14 @@
 Plots -f MainSel2016MuoLin.steer
 Plots -f MainSel2016MuoLin_DS.steer
 Plots -f MainSel2016MuoLinSingleEPS.steer
-Plots -f MainSel2016MuoLin_ttbarScaled.steer
-Plots -f MainSel2016MuoLinSingleEPS_ttbarScaled.steer
+Plots -f MainSel2016MuoLinSingleEPS_DS.steer
+#Plots -f MainSel2016MuoLin_ttbarScaled.steer
+#Plots -f MainSel2016MuoLinSingleEPS_ttbarScaled.steer
 Plots -f MainSel2016MuoLinNorm.steer
+Plots -f MainSel2016MuoLinNorm_DS.steer
 Plots -f MainSel2016MuoLinNormSingleEPS.steer
+Plots -f MainSel2016MuoLinNormSingleEPS_DS.steer
 Plots -f MatchHists2016MuoLin.steer
+Plots -f MatchHists2016MuoLin_DS.steer
 Plots -f MatchHists2016MuoLinSingleEPS.steer
+Plots -f MatchHists2016MuoLinSingleEPS_DS.steer
