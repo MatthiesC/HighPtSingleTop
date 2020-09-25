cb3a8a91d473df708072bc16f15c461291ef08bf
Christopher Matthies <Christopher.Matthies@desy.de>
2020-09-25 14:57:37 +0200

    no more low/high boost DNN hists

--- src/AndHists.cxx
+++ src/AndHists.cxx
@@ -110,8 +110,8 @@ BinnedDNNHists::BinnedDNNHists(Context & ctx, const string & dirname, const vect
   hist_toptag_pt = book<TH1F>("toptag_pt", "t jet p_{T} [GeV]", MyConstants::pt_binning.size(), MyConstants::pt_binning_edges);
 
   hists_vector.push_back(new DNNHists(ctx, dirname+"_Full", inputs, inputs_info, "DNN_TopTagPt"));
-  hists_vector.push_back(new DNNHists(ctx, dirname+"_Pt200to350", inputs, inputs_info, "DNN_TopTagPt", 0, 350));
-  hists_vector.push_back(new DNNHists(ctx, dirname+"_Pt350toInf", inputs, inputs_info, "DNN_TopTagPt", 350));
+  // hists_vector.push_back(new DNNHists(ctx, dirname+"_Pt200to350", inputs, inputs_info, "DNN_TopTagPt", 0, 350));
+  // hists_vector.push_back(new DNNHists(ctx, dirname+"_Pt350toInf", inputs, inputs_info, "DNN_TopTagPt", 350));
   for(auto i : MyConstants::pt_binning) {
     hists_vector.push_back(new DNNHists(ctx, dirname+"_Pt"+to_string((int)(i.first))+"to"+to_string((int)(i.second)), inputs, inputs_info, "DNN_TopTagPt", i.first, i.second));
   }
