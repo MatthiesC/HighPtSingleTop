49f0553f7bcc21cac44c520411b8899ff093384f
Christopher Matthies <Christopher.Matthies@desy.de>
2020-11-23 04:01:50 +0100

    update for new pathing

--- scripts/hadd_mainsel_2016_muo.sh
+++ scripts/hadd_mainsel_2016_muo.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 shopt extglob
 
-workdir=${CMSSW_BASE}/src/UHH2/HighPtSingleTop/output/MainSelection/2016/Muon/
+workdir=${CMSSW_BASE}/src/UHH2/HighPtSingleTop/output/mainsel/2016/muo/nominal/
 hadddir=hadded/
 preFix=uhh2.AnalysisModuleRunner.
 
@@ -15,13 +15,16 @@ hadd -f ${workdir}${hadddir}${preFix}MC.DYJets.root ${workdir}/${preFix}MC.DYJet
 hadd -f ${workdir}${hadddir}${preFix}MC.WJetsLight.root ${workdir}/${preFix}MC.WJetsLight*.root
 hadd -f ${workdir}${hadddir}${preFix}MC.WJetsHeavy.root ${workdir}/${preFix}MC.WJetsHeavy*.root
 hadd -f ${workdir}${hadddir}${preFix}MC.TTbar.root ${workdir}/${preFix}MC.TTbar*.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_non-tW.root ${workdir}/${preFix}MC.ST_non-tW*.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR.root ${workdir}/${preFix}MC.ST_tW_DR*.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_Sig.root ${workdir}/${preFix}MC.ST_tW_DR_@(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_Bkg.root ${workdir}/${preFix}MC.ST_tW_DR_!(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS.root ${workdir}/${preFix}MC.ST_tW_DS*.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS_Sig.root ${workdir}/${preFix}MC.ST_tW_DS_@(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
-hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS_Bkg.root ${workdir}/${preFix}MC.ST_tW_DS_!(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_non-tW.root ${workdir}/${preFix}MC.ST_tChannel*.root ${workdir}/${preFix}MC.ST_sChannel*.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_NoFullyHadronic.root ${workdir}/${preFix}MC.ST_tW_DR_NoFullyHadronic*.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_NoFullyHadronic_Sig.root ${workdir}/${preFix}MC.ST_tW_DR_NoFullyHadronic_@(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_NoFullyHadronic_Bkg.root ${workdir}/${preFix}MC.ST_tW_DR_NoFullyHadronic_!(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_inclusiveDecays.root ${workdir}/${preFix}MC.ST_tW_DR_inclusiveDecays*.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_inclusiveDecays_Sig.root ${workdir}/${preFix}MC.ST_tW_DR_inclusiveDecays_@(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DR_inclusiveDecays_Bkg.root ${workdir}/${preFix}MC.ST_tW_DR_inclusiveDecays_!(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS_NoFullyHadronic.root ${workdir}/${preFix}MC.ST_tW_DS_NoFullyHadronic*.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS_NoFullyHadronic_Sig.root ${workdir}/${preFix}MC.ST_tW_DS_NoFullyHadronic_@(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
+hadd -f ${workdir}${hadddir}${preFix}MC.ST_tW_DS_NoFullyHadronic_Bkg.root ${workdir}/${preFix}MC.ST_tW_DS_NoFullyHadronic_!(TopToHad_WToMuo|TopToMuo_WToHad)_T@(|bar)_2016v3.root
 
 shopt -u extglob
 shopt extglob
